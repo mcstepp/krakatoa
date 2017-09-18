550418524a31aa55075423d57956c1c6d0d6e53d
Meg Stepp <mcstepp@users.noreply.github.com>
2017-09-18 19:13:43 -0400

    forum view wip

--- angular/app/forum-view/forum-view.component.ts
+++ angular/app/forum-view/forum-view.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-forum-view',
   templateUrl: './forum-view.component.html',
   styleUrls: ['./forum-view.component.scss']
 })
 export class ForumViewComponent implements OnInit {
+
+  threads: Object[] = [
+      {name: 'Thread Titles 1',
+          description: 'Aliquam enim turpis, facilisis at dapibus a.',
+          author: 'John Doe Ray'},
+      {name: 'Thread Titles 2',
+          description: 'Aliquam enim turpis, facilisis at dapibus a.',
+          author: 'John Doe Ray'},
+      {name: 'Thread Titles 3',
+          description: 'Aliquam enim turpis, facilisis at dapibus a.',
+          author: 'John Doe Ray'},
+      {name: 'Thread Titles 4',
+          description: 'Aliquam enim turpis, facilisis at dapibus a.',
+          author: 'John Doe Ray'},
+      {name: 'Thread Titles 5',
+          description: 'Aliquam enim turpis, facilisis at dapibus a.',
+          author: 'John Doe Ray'},
+      {name: 'Thread Titles 6',
+          description: 'Aliquam enim turpis, facilisis at dapibus a.',
+          author: 'John Doe Ray'},
+      {name: 'Thread Titles 7',
+          description: 'Aliquam enim turpis, facilisis at dapibus a.',
+          author: 'John Doe Ray'},
+      {name: 'Thread Titles 8',
+          description: 'Aliquam enim turpis, facilisis at dapibus a.',
+          author: 'John Doe Ray'},
+      {name: 'Thread Titles 9',
+          description: 'Aliquam enim turpis, facilisis at dapibus a.',
+          author: 'John Doe Ray'}
+  ];
 
   constructor() { }
 
